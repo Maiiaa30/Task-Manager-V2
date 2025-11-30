cc6310de91ee528b275d83fcd79364f61f85af48
Maia <diogomaia2003@gmail.com>
2025-11-30 15:41:51 +0000

    Some fix on desing

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,15 +4,15 @@ import TasksIcon from "../assets/icons/tasks.svg?react";
 
 const SideBar = () => {
   return (
-    <div className="h-screen w-64 bg-white ">
-      <div className="px-8 py-6 space-y-4">
-        <h1 className="text-[#00ADB5] text-xl font-semibold">Task Manager</h1>
+    <div className="h-screen w-72 bg-white">
+      <div className="space-y-4 px-8 py-6">
+        <h1 className="text-xl font-semibold text-[#00ADB5]">Task Manager</h1>
         <p>
           Um simples{" "}
           <span className="text-[#00ADB5]"> organizador de tarefas</span>
         </p>
       </div>
-      <div className="flex flex-col p-2 gap-2">
+      <div className="flex flex-col gap-2 p-2">
         <SideBarButton variant={"unselect"}>
           <HomeIcon />
           Inicio
