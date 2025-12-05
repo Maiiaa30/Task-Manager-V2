9e9f7d0dab39787f32d5fc50cbc4750ea6e9283f
Maia <diogomaia2003@gmail.com>
2025-12-05 18:05:23 +0000

    Ensure task is create on DB

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -61,7 +61,7 @@ const TaskItem = ({ task, handleCheckboxClick, handleDeleteClick }) => {
 
 TaskItem.propTypes = {
   task: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string,
     timeOfDay: PropTypes.oneOf(["morning", "afternoon", "evening"]).isRequired,
