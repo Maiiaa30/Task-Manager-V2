84f6c7f519cca18c9600b112de852d4f8d79599c
Maia <diogomaia2003@gmail.com>
2025-12-19 22:24:04 +0000

    change the fetch to axios

--- src/hooks/data/use-get-task.js
+++ src/hooks/data/use-get-task.js
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export const useGetTask = ({ taskId, onSuccess }) => {
   return useQuery({
     queryKey: ["task", taskId],
     queryFn: async () => {
-      const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
-        method: "GET",
-      });
-      const data = await response.json();
+      const { data: data } = await axios.get(
+        `http://localhost:3000/tasks/${taskId}`,
+      );
       onSuccess(data);
       return data;
     },
--- src/hooks/data/use-update-task.js
+++ src/hooks/data/use-update-task.js
@@ -1,35 +1,28 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export const useUpdateTask = (taskId) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["updateTask", taskId],
     mutationFn: async (data) => {
-      const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
+      const { data: updatedTask } = await axios.patch(
+        `http://localhost:3000/tasks/${taskId}`,
+        {
           title: data.title.trim(),
           description: data.description.trim(),
           time: data.time,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error();
-      }
-      const updatedTask = await response.json();
-      queryClient.setQueryData(["task", taskId], updatedTask);
-      queryClient.setQueryData(["tasks"], (oldData) => {
-        if (!oldData) return oldData;
-        return oldData.map((oldTask) => {
-          if (oldTask.id === taskId) {
+        },
+      );
+      queryClient.setQueryData("tasks", (oldTasks) => {
+        return oldTasks.map((task) => {
+          if (task.id === taskId) {
             return updatedTask;
           }
-          return oldTask;
+          return task;
         });
       });
+      queryClient.setQueryData(["task", taskId], updatedTask);
     },
   });
 };
