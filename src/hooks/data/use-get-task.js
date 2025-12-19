e2c9e924d75bd66da4e794fd2697acebd82e2490
Maia <diogomaia2003@gmail.com>
2025-12-19 23:01:23 +0000

    Add task query variable

--- src/hooks/data/use-get-task.js
+++ src/hooks/data/use-get-task.js
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { tasksQueryKeys } from "../../keys/queries";
 import { api } from "../../lib/axios";
 
 export const useGetTask = ({ taskId, onSuccess }) => {
   return useQuery({
-    queryKey: ["task", taskId],
+    queryKey: tasksQueryKeys.getOne(taskId),
     queryFn: async () => {
       const { data: data } = await api.get(`/tasks/${taskId}`);
       onSuccess(data);
